7d1d7a313cf53473ab3b778b4327b8d87a71bcfe
Saeid Nourian <snourian@concord.org>
2017-05-02 11:30:17 -0400

    Fetch map coordinates of an address Fetch map image of a coordinate Update map image when the user changes address or altitude/longitude

--- public_html/energy3d/Sidebar.Map.js
+++ public_html/energy3d/Sidebar.Map.js
@@ -6,6 +6,36 @@
 
 Sidebar.Map = function (editor) {
 
+	function updateMap() {
+		mapImg.src = getGoogleMapUrl(false);
+	}
+
+	function getGoogleMapUrl(highResolution) {
+		let x = parseFloat(latitudeInput.getValue());
+		let y = parseFloat(longitudeInput.getValue());
+		let zoom = parseFloat(zoomInput.getValue());
+		let scale = highResolution & zoom <= 20 ? 2 : 1;
+		return "https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=" + x + "," + y + "&zoom=" + zoom + "&size=640x640&scale=" + scale + "&key=AIzaSyBEGiCg33CccHloDdPENWk1JDhwTEQaZQ0";
+}
+
+	function getGoogleMapAddressCoordinates() {
+		const address = addressInput.getValue().replace(' ', '+');
+		const url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyD7MfCQjMAlsdFA3OmfGZ_rzC8ldJPnoHc";
+		const xmlHttp = new XMLHttpRequest();
+		xmlHttp.onreadystatechange = function () {
+			if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+				const result = JSON.parse(xmlHttp.responseText).results[0];
+				addressInput.setValue(result.formatted_address);
+				latitudeInput.setValue(result.geometry.location.lat);
+				longitudeInput.setValue(result.geometry.location.lng);
+				zoomInput.setValue(20);
+				updateMap();
+			}
+		};
+		xmlHttp.open("GET", url, true); // true for asynchronous
+		xmlHttp.send(null);
+	}
+
 	var container = new UI.Panel();
 	container.setBorderTop('0');
 	container.setPaddingTop('20px');
@@ -13,7 +43,7 @@ Sidebar.Map = function (editor) {
 	// address
 	var row = new UI.Row();
 	var addressInput = new UI.Input("25 Love lane, Concord, MA, USA").setWidth('210px').onChange(function () {
-		editor.signals.sceneGraphChanged.dispatch();
+		getGoogleMapAddressCoordinates();
 	});
 
 	addressInput.setMargin("5px");
@@ -24,14 +54,14 @@ Sidebar.Map = function (editor) {
 
 	// latitude, longitude, zoom
 	var row = new UI.Row();
-	var latitudeInput = new UI.Input("42").setWidth('27px').onChange(function () {
-		editor.signals.sceneGraphChanged.dispatch();
+	var latitudeInput = new UI.Input("42.45661").setWidth('27px').onChange(function () {
+		updateMap();
 	});
-	var longitudeInput = new UI.Input("-72").setWidth('27px').onChange(function () {
-		editor.signals.sceneGraphChanged.dispatch();
+	var longitudeInput = new UI.Input("-71.35823").setWidth('27px').onChange(function () {
+		updateMap();
 	});
 	var zoomInput = new UI.Input("20").setWidth('27px').onChange(function () {
-		editor.signals.sceneGraphChanged.dispatch();
+		updateMap();
 	});
 
 	latitudeInput.setMargin("5px");
@@ -49,7 +79,21 @@ Sidebar.Map = function (editor) {
 	// map image
 	var row = new UI.Row();
 	var mapImg = document.createElement('img');
-	row.add(mapImg);
+	mapImg.style.width = "100%";
+	mapImg.src = getGoogleMapUrl(false);
+	row.dom.appendChild(mapImg);
+	container.add(row);
+
+	// apply button
+	var applyButton = new UI.Button("Apply");
+	var clearButton = new UI.Button("Clear");
+
+	applyButton.setMargin("5px");
+	clearButton.setMargin("5px");
+
+	var row = new UI.Row();
+	row.add(applyButton);
+	row.add(clearButton);
 	container.add(row);
 
 	return container;
